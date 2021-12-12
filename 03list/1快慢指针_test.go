7083e759c393f9c14cdb7ecf0a4b8da4421c5b21
donshanlang <1044052828@qq.com>
2021-12-12 16:55:43 +0800

    method: get middle or down middle node

--- 03list/1快慢指针_test.go
+++ 03list/1快慢指针_test.go
@@ -55,3 +55,33 @@ func getMidOrUpMid(node *Node) *Node {
 	}
 	return slowPointer
 }
+
+//2. mid or down mid node
+func getMidOrDownMid(head *Node) *Node {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	slowPointer := head
+	fastPointer := head.Next
+	for fastPointer.Next != nil && fastPointer.Next.Next != nil {
+		slowPointer = slowPointer.Next
+		fastPointer = fastPointer.Next.Next
+	}
+	return slowPointer.Next
+}
+func TestMidOrDownMid(t *testing.T) {
+	n1 := &Node{Value: 1}
+	n2 := &Node{Value: 2}
+	//n3 := &Node{Value: 3}
+	//n4 := &Node{Value: 4}
+	//n5 := &Node{Value: 5}
+	//n6 := &Node{Value: 6}
+
+	n1.Next = n2
+	//n2.Next = n3
+	//n3.Next = n4
+	//n4.Next = n5
+	//n5.Next = n6
+
+	fmt.Println(getMidOrDownMid(n1))
+}
