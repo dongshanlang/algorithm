ee8d961747bffa6cafa6995e6559036826e1ccb3
lixiumin <lixiumin@qie.tv>
2021-12-20 14:08:26 +0800

    暴力求解两种袋子装苹果

--- 5table_and_matrix/1两种容器装苹果_test.go
+++ 5table_and_matrix/1两种容器装苹果_test.go
@@ -7,6 +7,11 @@
 
 package _table_and_matrix
 
+import (
+	"fmt"
+	"testing"
+)
+
 //小虎区买苹果，商店只提供两种类型的塑料袋，每种类型都有任意数量。
 //1）能装下6个苹果
 //2）能装下8个苹果
@@ -14,3 +19,28 @@ package _table_and_matrix
 //给定一个正整数N，返回至少使用多少个袋子，如果N无法让使用的每个袋子必须装满，返回-1
 
 //6，8，12，18，24
+func violentApple(n int) (six, eight int) {
+	if n < 6 {
+		return -1, -1
+	}
+	eight = n / 8
+	eightRest := n % 8
+	for eightRest < 24 && eight >= 0 {
+		if eightRest == 0 {
+			return eight, 0
+		}
+		if eightRest%6 == 0 {
+			return eight, eightRest / 6
+		} else {
+			eight = eight - 1
+			eightRest = n - eight*8
+		}
+	}
+	return -1, -1
+}
+func TestViolentApple(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		six, eight := violentApple(i)
+		fmt.Printf("%d: %d %d %d\n", i, six, eight, six+eight)
+	}
+}
