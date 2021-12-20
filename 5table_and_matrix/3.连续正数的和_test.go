1ab87146f6af9727df32c4201fcb396706cfcdb1
lixiumin <lixiumin@qie.tv>
2021-12-20 15:57:04 +0800

    暴力求解连续正数的和

--- 5table_and_matrix/3.连续正数的和_test.go
+++ 5table_and_matrix/3.连续正数的和_test.go
@@ -7,6 +7,11 @@
 
 package _table_and_matrix
 
+import (
+	"fmt"
+	"testing"
+)
+
 //定义一种数：可以表示成若干（数量>1）连续正数和的数
 //比如：
 //5=2+3,5就是这样的数
@@ -14,3 +19,26 @@ package _table_and_matrix
 //1不是这样的数，因为要求数量大于1个、连续正数和
 //2=1+1,2也不是，因为等号右边不是连续正数
 //给定一个参数，返回是不是可以表示成若干连续正数和的数
+func getContinuousNumber(n int) bool {
+	for i := 1; i <= n/2; i++ {
+		sum := 0
+		j := i
+		for ; sum <= n; j++ {
+			sum += j
+			if sum == n {
+				fmt.Println(i, " ", j)
+				return true
+			}
+		}
+
+	}
+	return false
+}
+func TestGetContinuousNumber(t *testing.T) {
+	fmt.Println(getContinuousNumber(12))
+}
+func TestGetContinuousNumberIn100(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(i, " ", getContinuousNumber(i))
+	}
+}
