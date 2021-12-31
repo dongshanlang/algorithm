1e85dc534a4694f2f0b014f6ce8da8e8fda24b7d
lixiumin <lixiumin@qie.tv>
2021-12-31 16:44:30 +0800

    graph breadth first search

---
+++ 7graph/1graph.go
@@ -0,0 +1,48 @@
+/**
+ * @Author: lixiumin
+ * @E-Mail: lixiuminmxl@163.com
+ * @Date: 2021/12/31 2:24 下午
+ * @Desc:
+ */
+
+package _graph
+
+import (
+	"algorithm/utils/mqueue"
+)
+
+type Graph struct {
+	Nodes []*node
+	Edges []*edge
+}
+type node struct {
+	ID        int
+	Value     int
+	In, Out   int
+	NextNodes *mqueue.MQueue
+	Edges     *mqueue.MQueue
+}
+
+func NewNode(value int) *node {
+	return &node{
+		ID:        0,
+		Value:     value,
+		In:        0,
+		Out:       0,
+		NextNodes: mqueue.New(),
+		Edges:     mqueue.New(),
+	}
+}
+
+type edge struct {
+	Value    int
+	From, To *node
+}
+
+func NewEdge(value int, from, to *node) *edge {
+	return &edge{
+		Value: value,
+		From:  from,
+		To:    to,
+	}
+}
---
+++ 7graph/2广度优先遍历_test.go
@@ -0,0 +1,77 @@
+/**
+ * @Author: lixiumin
+ * @E-Mail: lixiuminmxl@163.com
+ * @Date: 2021/12/31 2:30 下午
+ * @Desc:
+ */
+
+package _graph
+
+import (
+	"algorithm/utils/mqueue"
+	"fmt"
+	"testing"
+)
+
+var (
+	n1 = NewNode(1)
+	n2 = NewNode(2)
+	n3 = NewNode(3)
+	n5 = NewNode(5)
+	n6 = NewNode(6)
+)
+
+func TestBreadthFirstSearch(t *testing.T) {
+	InitGraph()
+	q := mqueue.New()
+	m := make(map[*node]bool)
+	m[n1] = true
+	q.PushBack(n1)
+	for !q.IsEmpty() {
+		curNode := q.PopFront().(*node)
+		fmt.Println(curNode.Value)
+		for next := curNode.NextNodes.PopFront(); next != nil; next = curNode.NextNodes.PopFront() {
+			nextNode := next.(*node)
+			if !m[nextNode] { //确保不会重复进队列
+				q.PushBack(next)
+				m[nextNode] = true
+			}
+		}
+	}
+}
+func InitGraph() {
+	//g := Graph{}
+	n1.NextNodes.PushBack(n2)
+	n1.Out++
+	n2.In++
+	n1.Edges.PushBack(NewEdge(1, n1, n2))
+	n1.NextNodes.PushBack(n3)
+	n1.Out++
+	n1.In++
+	n1.Edges.PushBack(NewEdge(3, n1, n3))
+
+	n2.NextNodes.PushBack(n5)
+	n2.Edges.PushBack(NewEdge(1, n2, n5))
+	n2.Out++
+	n5.In++
+
+	n3.NextNodes.PushBack(n6)
+	n3.Edges.PushBack(NewEdge(3, n3, n6))
+	n3.Out++
+	n6.In++
+
+	n5.NextNodes.PushBack(n3)
+	n5.Edges.PushBack(NewEdge(2, n5, n3))
+	n5.Out++
+	n3.In++
+
+	n5.NextNodes.PushBack(n6)
+	n5.Edges.PushBack(NewEdge(2, n5, n6))
+	n5.Out++
+	n6.In++
+
+	n3.NextNodes.PushBack(n1)
+	n3.Edges.PushBack(NewEdge(1, n3, n1))
+	n3.Out++
+	n1.In++
+}
