6f2f6056c3b4ddc9d4f6f4e4e0ac2d00e6411787
lixiumin <lixiumin@qie.tv>
2022-01-18 16:25:42 +0800

    获取中间数值

--- 100array/1数组旋转_test.go
+++ 100array/1数组旋转_test.go
@@ -27,26 +27,29 @@ var (
 	arr2    = []int{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
 	target2 = 11
 
-//输出：-1 （没有找到）
+	//输出：-1 （没有找到）
+	//输入：
+	arr3    = []int{1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1}
+	target3 = 2
 )
 
 func TestArray(t *testing.T) {
-	fmt.Println(search(arr1, target1))
-	fmt.Println(search(arr2, target2))
+	fmt.Println(searchMinIndex(arr3))
 }
-func search(arr []int, target int) int {
-	return recursiveSearch(arr, 0, len(arr)-1, target)
-}
-func recursiveSearch(arr []int, left, right, target int) int {
-	if left == right {
-		if arr[left] == target {
-			return left
+func searchMinIndex(arr []int) int {
+	n := len(arr)
+	for n > 1 && arr[n-1] == arr[0] {
+		n--
+	}
+	left := 0
+	right := n - 1
+	for left < right {
+		mid := (left + right) / 2
+		if arr[mid] <= arr[right] {
+			right = mid
+		} else {
+			left = mid + 1
 		}
-		return -1
 	}
-	mid := left + (right-left)/2
-	if arr[left] < arr[mid] && target > arr[left] && target <= arr[mid] {
-		return recursiveSearch(arr, left, mid, target)
-	}
-	return recursiveSearch(arr, mid+1, right, target)
+	return left
 }
