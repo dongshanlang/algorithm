7ae88016f7d1b7f592287a7d5a79904b9e2611f6
lixiumin <lixiumin@qie.tv>
2022-03-21 15:11:41 +0800

    type: feat desc: level order traversal

--- 04binary_tree/3level_traversal_and_biggest_width_test.go
+++ 04binary_tree/3level_traversal_and_biggest_width_test.go
@@ -72,3 +72,65 @@ func TestGetMostWidthOfTree(t *testing.T) {
 	max := getBiggestWidthOfTree(n1)
 	fmt.Println(max)
 }
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func TestLevelOrder(t *testing.T) {
+	n1 := &TreeNode{Val: 1}
+	n2 := &TreeNode{Val: 2}
+	n3 := &TreeNode{Val: 3}
+	n4 := &TreeNode{Val: 4}
+	n5 := &TreeNode{Val: 5}
+	n6 := &TreeNode{Val: 6}
+	n7 := &TreeNode{Val: 7}
+	n8 := &TreeNode{Val: 8}
+	n9 := &TreeNode{Val: 9}
+	n1.Left = n2
+	n1.Right = n3
+	n2.Left = n4
+	n2.Right = n5
+	n3.Left = n6
+	n3.Right = n7
+	n5.Left = n8
+	n5.Right = n9
+	ret := levelOrder(n1)
+	fmt.Println(ret)
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	// write code here
+	if root == nil {
+		return nil
+	}
+	result := make([][]int, 0)
+	result = append(result, make([]int, 0))
+	currentLevelEnd := root
+	var nextLevelEnd *TreeNode
+	currentLevel := 0
+	nodeList := make([]*TreeNode, 0)
+	nodeList = append(nodeList, root)
+
+	for i := 0; i < len(nodeList); i++ {
+		result[currentLevel] = append(result[currentLevel], nodeList[i].Val)
+		if nodeList[i].Left != nil {
+			nodeList = append(nodeList, nodeList[i].Left)
+			nextLevelEnd = nodeList[i].Left
+		}
+		if nodeList[i].Right != nil {
+			nodeList = append(nodeList, nodeList[i].Right)
+			nextLevelEnd = nodeList[i].Right
+		}
+		if nodeList[i] == currentLevelEnd {
+			currentLevel++
+			currentLevelEnd = nextLevelEnd
+			nextLevelEnd = nil
+			result = append(result, make([]int, 0))
+		}
+	}
+	fmt.Println(currentLevel)
+	return result[0 : len(result)-1]
+}
